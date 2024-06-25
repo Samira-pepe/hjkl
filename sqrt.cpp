b380ecd66fe28148cbb6b6afd2281ab57adf780c
Samira-pepe <vincenze.sky@gmail.com>
2024-06-25 14:53:59 +0300

    Add files via upload

---
+++ sqrt.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+int main(){
+    int x=13;
+    int y=7;
+    int sqrt1=sqrt(x);
+    int other_sqrt=sqrt(y);
+    std::cout<<"Корень:"<<sqrt1<<std::endl;
+    std::cout<<"Другой корень:"<<other_sqrt<<std::endl;
+}
