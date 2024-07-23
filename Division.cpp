3e0dbc8de8a09a025f49a2bbdc2854f4a3dfbfbc
Samira-pepe <vincenze.sky@gmail.com>
2024-07-23 23:28:49 +0300

    Add files via upload

--- Division.cpp
+++ Division.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 int main(){
-    int x=14;
-    int y=7;
+    int x=31;
+    int y=3;
     int quot={x/y};
     int other_quot={x/2};
     std::cout<<"Частное:"<<quot<<std::endl;
     std::cout<<"Другое частное:"<<other_quot<<std::endl;
 }
+
